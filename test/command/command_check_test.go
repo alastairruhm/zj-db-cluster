8a6dfe006a0244952153afc0912f02e745cc33ca
alastairruhm <alastairruhm@gmail.com>
2017-12-01 15:01:53 +0800

    fix: fix test problem no non-test Go files in <dirs>

---
+++ test/command/command_check_test.go
@@ -0,0 +1,52 @@
+package command
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/alastairruhm/zj-db-cluster/cmd"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCheckCmd(t *testing.T) {
+	rootCmd := cmd.RootCmd
+	Convey("Test check command", t, func() {
+		checkCmd := cmd.CheckCmd
+
+		Convey("[zj-db-cluster check]: show usage of check", func() {
+			buf := new(bytes.Buffer)
+			rootCmd.SetArgs([]string{"check"})
+			rootCmd.SetOutput(buf)
+
+			err := rootCmd.Execute()
+
+			if err != nil {
+				t.Error(err)
+			}
+
+			actual := buf.String()
+			expected := checkCmd.UsageString()
+			So(actual, ShouldEqual, expected)
+		})
+	})
+
+	// Convey("Test check connection command", t, func() {
+
+	// 	Convey("", func() {
+	// 		buf := new(bytes.Buffer)
+	// 		rootCmd.SetArgs([]string{"check", "connection"})
+	// 		rootCmd.SetOutput(buf)
+
+	// 		err := rootCmd.Execute()
+
+	// 		if err != nil {
+	// 			t.Error(err)
+	// 		}
+
+	// 		actual := buf.String()
+	// 		expected := checkCmd.UsageString()
+	// 		So(actual, ShouldEqual, expected)
+	// 	})
+	// })
+
+}
